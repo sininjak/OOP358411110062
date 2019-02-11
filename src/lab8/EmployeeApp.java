975b75f19e65e342bbe95c5664d846564b577556
sutinan <sutinan2820@gmail.com>
2019-02-11 14:19:56 +0700

    lab8

--- src/lab8/EmployeeApp.java
+++ src/lab8/EmployeeApp.java
@@ -3,7 +3,7 @@ package lab8;
 public class EmployeeApp {
     public static void main(String[] args) {
         //create objects
-        Employee emp = new Employee("s35841111006","Sutinan saningjac","student",50000.00);
+        Employee emp = new Employee("s358411110062","Sutinan saninjak","student",50000.00);
 
         System.out.print(emp.toString());
 
